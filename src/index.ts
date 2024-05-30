76439b63dc5df68c756875e8a7dfb920eeb6813e
narinthonradiant1 <155409348+narinthonradiant1@users.noreply.github.com>
2024-05-30 21:39:57 +0700

    [add] cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import todoRoutes from "./routes/todo";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/todos", todoRoutes);
 
@@ -12,6 +15,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).send("Not Found");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
